6f12409013d6e525993f0916fc6ff7c1586733f3
AC05 <oderocharlesouma@gmail.com>
2019-07-03 15:03:52 +0300

    Third commit

---
+++ app/src/main/java/www/grif/calculator/MainActivity.java
@@ -0,0 +1,34 @@
+package www.grif.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   private EditText   editText;
+   private TextView  textView;
+   private  Button   buttonZero;
+   private  Button  buttonOne;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
