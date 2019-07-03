89c2657c3a86dab911986a77ed6fdfb9f8b65cb6
AC05 <oderocharlesouma@gmail.com>
2019-07-03 16:29:02 +0300

    third commit

--- app/src/main/java/www/grif/calculator/MainActivity.java
+++ app/src/main/java/www/grif/calculator/MainActivity.java
@@ -2,16 +2,15 @@ package www.grif.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-   private EditText   editText;
-   private TextView  textView;
-   private  Button   buttonZero;
-   private  Button  buttonOne;
+  Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9,
+    buttonc,button10, buttonadd, buttonsub, buttonmultiply, buttondiv, buttonequal
 
 
 
@@ -30,5 +29,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonzero = (Button)findViewById(R.id.bt_button0_id);
+        buttonzero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "0");
+            }
+        });
+        Button buttonone = (Button)findViewById(R.id.bt_button1_id);
+        buttonone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() +"1");
+            }
+        });
+
+
     }
+
+
 }
